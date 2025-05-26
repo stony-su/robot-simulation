02efa315147ba62c1922781a4bc817ecb76ab7a5
Wenxuan Su <35317989+stony-su@users.noreply.github.com>
2025-05-26 09:28:19 -0400

    Update Application.java

--- src/robotSim/Application.java
+++ src/robotSim/Application.java
@@ -3,14 +3,19 @@ package robotSim;
 public class Application {
 	final static int OCTO_NUM = 1;
 	final static int PLAYER_NUM = 10;	
+	static boolean allPlayersCaught = false;
 	
 	public static void main (String []args) {
 		Player [] playerArr = new Player [PLAYER_NUM + OCTO_NUM];
-		while (true) {
+		while (!allPlayersCaught) {
 			for (int i = 0; i < playerArr.length; i++) {
 				playerArr[i].move();
-			}
+			}	
 			
 		}
+	}
+	
+	public void updateStatus (boolean allPlayersCaught) {
+		this.allPlayersCaught = allPlayersCaught;
 	}
 }
