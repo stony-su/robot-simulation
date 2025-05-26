f40b2aa77fbc8f545cf13cc1217f6533945fb45e
Wenxuan Su <35317989+stony-su@users.noreply.github.com>
2025-05-26 09:17:28 -0400

    player2

--- src/robotSim/Player.java
+++ src/robotSim/Player.java
@@ -1,5 +1,13 @@
 package robotSim;
 
 public abstract class Player {
+	int energyLevel;
+	int stepsPerMove;
+	int dodgingAbility;
+	String name;
+
+	playerRecord [] playerList;
+	
+	abstract public void move();
 
 }
