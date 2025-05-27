29a01a39158a2ad693de14398c8fddfd36bd1af3
blaze <Arnnav0kudale@gmail.com>
2025-05-27 08:33:16 -0400

    player abstract class

--- src/robotSim/Player.java
+++ src/robotSim/Player.java
@@ -1,17 +1,54 @@
 package robotSim;
-import java.awt.Color;
 
 public abstract class Player {
-	int energyLevel;
-	int maximumEnergyLevel;
-	int stepsPerMove;
-	int dodgingAbility;
-	String name;
+    private int energyLevel;
+    private int stepsPerMove;
+    private int dodgingAbility;
+    private String name;
 
-	playerRecord [] playerList;
-	
-	abstract public void move();
-	abstract public void setColor(Color color);
-	abstract public String getType();
+    protected playerRecord[] playerList;
 
+    public Player(String name, int energyLevel, int stepsPerMove, int dodgingAbility, playerRecord[] playerList) {
+        this.name = name;
+        this.energyLevel = energyLevel;
+        this.stepsPerMove = stepsPerMove;
+        this.dodgingAbility = dodgingAbility;
+        this.playerList = playerList;
+    }
+
+    public abstract void move();
+    public abstract void setColor();
+    public abstract String getType();
+
+    public int getEnergyLevel() {
+        return energyLevel;
+    }
+
+    public void setEnergyLevel(int energyLevel) {
+        this.energyLevel = energyLevel;
+    }
+
+    public int getStepsPerMove() {
+        return stepsPerMove;
+    }
+
+    public void setStepsPerMove(int stepsPerMove) {
+        this.stepsPerMove = stepsPerMove;
+    }
+
+    public int getDodgingAbility() {
+        return dodgingAbility;
+    }
+
+    public void setDodgingAbility(int dodgingAbility) {
+        this.dodgingAbility = dodgingAbility;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
