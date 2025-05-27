4c4c2c1f0844f3428708906e5c76366f484c331c
GoldenBlock99 <1rahmanmay@hdsb.ca>
2025-05-27 08:30:08 -0400

    Update playerRecord.java
    added speed attribute

--- src/robotSim/playerRecord.java
+++ src/robotSim/playerRecord.java
@@ -4,15 +4,17 @@ public class playerRecord {
     private int enemyX;
     private int enemyY;
     private int dodgeAbility;
+    private int speed;
     private String name;
     private String type;
 
-    public playerRecord(int enemyX, int enemyY, String name, String type) {
+    public playerRecord(int enemyX, int enemyY, String name, String type, int dodgeAbility, int speed) {
         this.enemyX = enemyX;
         this.enemyY = enemyY;
         this.name = name;
         this.type = type;
-        this.dodgeAbility = 5;
+        this.dodgeAbility = dodgeAbility;
+        this.speed = speed;
     }
 
     public int getDodge() {
@@ -34,10 +36,15 @@ public class playerRecord {
     public String getType() {
         return type;
     }
+    
+    public int getSpeed() {
+    	return speed;
+    }
 
-    public void updateRecord(int enemyX, int enemyY, int dodgeAbility) {
+    public void updateRecord(int enemyX, int enemyY, int dodgeAbility, int speed) {
         this.enemyX = enemyX;
         this.enemyY = enemyY;
         this.dodgeAbility = dodgeAbility;
+        this.speed = speed;
     }
 }
