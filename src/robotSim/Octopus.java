31ce1f2284d105c75f0bab0833c71ef2b34a5fe5
GoldenBlock99 <1rahmanmay@hdsb.ca>
2025-05-28 08:53:17 -0400

    Update Octopus.java

--- src/robotSim/Octopus.java
+++ src/robotSim/Octopus.java
@@ -3,34 +3,95 @@ package robotSim;
 import java.awt.*;
 import becker.robots.City;
 import becker.robots.Direction;
+import java.util.*;
 
 public class Octopus extends Player {
 	private int x, y;
-	private int maximumEnergyLevel, stepsPerMove;
+	private int maximumEnergyLevel, stepsPerMove, energyLevel;
 	private double dodgingAbility;
 	private playerRecord[] playerList;
 	private String name;
-	public Octopus(String name, int energyLevel, int stepsPerMove, double dodgingAbility	, City city, int y, int x, Direction direction) {
-		super(name,energyLevel, stepsPerMove, dodgingAbility, city, y, x, direction);
-		setColor(new Color(255, 165, 0));
-	}
-
-	@Override
-	public void move() {
-		// TODO Auto-generated method stub
-
-	}
-
+	private boolean resting = false;
+	private int targetX, targetY;
+	private double targetDistance;
+	private String targetName;
+	
 	
 
-	public int getType() {
+	public Octopus(String name, int energyLevel, int stepsPerMove, double dodgingAbility, playerRecord[] playerList, City city, int y, int x, Direction direction) {
+		super(name,energyLevel, stepsPerMove, dodgingAbility, city, y, x, direction);
+		this.setColor(new Color(255, 165, 0));
+		this.maximumEnergyLevel = energyLevel;
+		this.energyLevel = this.maximumEnergyLevel;
 
+	}
+	
+	
+
+	
+	public void takeTurn() {
+		if (this.sufficientEnergy() && this.resting != true) {
+			this.chase();
+		} else {
+			this.rest();
+		}
+
+	}
+	
+	private void chase() {
+		this.lockOnTarget();
+		this.advanceToTarget();
+		this.energyLevel -= 1;
+		this.tagAttempt();
+	}
+	
+	
+	private void tagAttempt() {
+		
+	}
+
+
+	private void advanceToTarget() {
+		
+	}
+
+
+	private void lockOnTarget() {
+		this.sortByDistance();
+	}
+
+
+	private void rest() {
+		Random r = new Random();
+		if (this.resting == true) {
+			for (int i =0; i < (r.nextInt(3 - 1 + 1) + 1); i++) {
+				if (this.energyLevel +1 <= this.maximumEnergyLevel) {
+					this.energyLevel += 1;
+				} else {
+					this.resting = false;
+				}
+			}
+		}
+		
+	}
+
+	private boolean sufficientEnergy() {
+		if (this.energyLevel > 0) {
+			return true;
+		} else {
+			return false;
+			
+		}
+	}
+
+
+	public int getType() {
 		return 4;
 	}
 
 	@Override
 	public void setColor(Color color) {
-		setColor(color);
+		super.setColor(color);
 	}
 
 	
