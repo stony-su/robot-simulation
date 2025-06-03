8b4ab08d4f505a1a1e8f2fe50bdefb28ab2f2672
GoldenBlock99 <1rahmanmay@hdsb.ca>
2025-06-03 08:18:37 -0400

    Update Octopus.java

--- src/robotSim/Octopus.java
+++ src/robotSim/Octopus.java
@@ -6,28 +6,49 @@ import becker.robots.Direction;
 import unit3.Account;
 
 import java.util.*;
-
+import java.util.*;
 public class Octopus extends Player {
+	Random gen = new Random();
 	private int x, y;
 	private int maximumEnergyLevel, maxStepsPerMove, energyLevel;
 	private double dodgingAbility;
-	private boolean chasing = false;
 	private playerRecord[] playerList;
 	private String name;
 	private boolean resting = false;
+	private boolean chasing = false;
+	private boolean tagging = false;
 	private int targetX, targetY;
 	private double targetDistance;
 	private String targetName;
 
 
 
-	public Octopus(String name, int energyLevel, int maxStepsPerMove, double dodgingAbility, City city, int y, int x, Direction direction) {
+	public Octopus(String name, int energyLevel, int maxStepsPerMove, double dodgingAbility, playerRecord[] playerList, City city, int y, int x, Direction direction) {
 		super(name,energyLevel, maxStepsPerMove, dodgingAbility, city, y, x, direction);
 		this.setColor(new Color(255, 165, 0));
 		this.maximumEnergyLevel = energyLevel;
 		this.energyLevel = this.maximumEnergyLevel;
+		
 
 	}
+
+	public void move() {
+
+		if (this.chasing) {
+			int stepsNum = this.gen.nextInt(maxStepsPerMove);
+			super.move(maxStepsPerMove);
+			if (stepsNum == maxStepsPerMove) {
+				this.energyLevel -= 2;
+			} else {
+				this.energyLevel -= 1;
+			}
+
+		} else {
+			super.move();
+		}
+	}
+
+
 
 	public void takeTurn() {
 		if (this.sufficientEnergy() && this.resting != true) {
@@ -39,27 +60,57 @@ public class Octopus extends Player {
 	}
 
 	private void chase() {
-		this.chasing= true;
 		this.lockOnTarget();
 		this.advanceToTarget();
-		this.energyLevel -= 1;
 		this.tagAttempt();
+		this.tagging = false;
+		this.rest();
 	}
 
 
 	private void tagAttempt() {
-
+		this.tagging = true;
+	}
+	
+	public boolean getTagging() {
+		return this.tagging;
 	}
 
-
 	private void advanceToTarget() {
+		if (this.targetX != x) {
+			if (this.targetX < x) {
+				this.faceWest();
+				for (int i = 0; i < (this.x-this.targetX); i++) {
+					this.move();
+				}
+			} else {
+				this.faceEast();
+				for (int i = 0; i < (this.targetX-this.x); i++) {
+					this.move();
+				}
+			}
 
+		} else if (this.targetY != this.y) {
+			if (this.targetY < y) {
+				this.faceNorth();
+				for (int i = 0; i < (this.y-this.targetY); i++) {
+					this.move();
+				}
+			} else {
+				this.faceSouth();
+				for (int i = 0; i < (this.targetY-this.y); i++) {
+					this.move();
+				}
+			}
+		}
 	}
 
 
 	private void lockOnTarget() {
-		if (distanceCalc(playerList[0]) > maxStepsPerMove/energyLevel) {
+		if (this.chasing == false) {
+			
 			this.sortByDistance(playerList);
+			this.chasing = true;
 			for (int i =0; i < playerList.length; i++) {
 				if (playerList[i].getType() < 3) {
 					this.targetY = playerList[i].getY();
@@ -67,7 +118,6 @@ public class Octopus extends Player {
 					this.targetName = playerList[i].getName();
 				}
 			}
-
 		}
 	}
 
@@ -128,5 +178,61 @@ public class Octopus extends Player {
 
 
 
+
+	public void setPlayerRecord(playerRecord [] arr) {
+		this.playerList = arr;
+
+	}
+
+
+	private void faceSouth() {
+		while (this.isFacingSouth() == false) {
+			this.turnLeft();
+		}
+	}
+	/**
+	 * General helper method, faces to the west. 
+	 * If it is not facing west, then it will turn left until it does.
+	 */
+	private void faceWest() {
+		while (this.isFacingWest() == false) {
+			this.turnLeft();
+		}
+	}
+
+
+	/**
+	 * General helper method, faces to the East. 
+	 * If it is not facing east, then it will turn left until it does.
+	 */
+	private void faceEast() {
+		while (this.isFacingEast() == false) {
+			this.turnLeft();
+		}
+	}
+
+
+	/**
+	 * General helper method, faces to the north.
+	 * If it isn't facing north, then it will turn right until it does.
+	 */
+	private void faceNorth() {
+		while (this.isFacingNorth() == false) {
+			this.turnRight();
+		}
+	}
+
+	@Override
+	public void setRunnerRecord(Player[] arr) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	
+	
+
+	
+	
 
 }
